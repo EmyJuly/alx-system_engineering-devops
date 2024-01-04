5ae6419641972a95a6975160926a49c184938def
root <farsimane00@gmail.com>
2024-01-03 17:14:27 -0800

    puppt

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,37 +1,21 @@
-#!/usr/bin/env bash
-#Install Nginx web server
+# Automating project requirements using Puppet
 
-#install nginx
-package {'nginx':
-  ensure   => 'installed',
-  provider => apt,
+package { 'nginx':
+  ensure => installed,
 }
 
-file {'/var/www/html/index.html':
-  ensure  => file,
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/EmyJuly permanent;',
+}
+
+file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
-service { 'nginx':
-  ensure => running,
-  enable => true,
-  require => Package['nginx'],
- }
 
-file {'/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => '
-server {
-    listen 80 ;
-    listen [::]:80;
-    root /var/www/html;
-    index index.html;
-    servername ;
-    location / {
-        try_files $uri $uri/ =404;
-    }
-    location /redirect_me {
-        return 301 /;
-    }
-}',
-  notify => Service['nginx'],
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
