24db9094e2ccb31701527f8e7be3b6acfc225a5a
root <farsimane00@gmail.com>
2024-01-03 17:09:43 -0800

    puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#Install Nginx web server
+
+#install nginx
+package {'nginx':
+  ensure   => 'installed',
+  provider => apt,
+}
+
+file {'/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+}
+service { 'nginx':
+  ensure => running,
+  enable => true,
+  require => Package['nginx'],
+ }
+
+file {'/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => '
+server {
+    listen 80 ;
+    listen [::]:80;
+    root /var/www/html;
+    index index.html;
+    servername ;
+    location / {
+        try_files $uri $uri/ =404;
+    }
+    location /redirect_me {
+        return 301 /;
+    }
+}',
+  notify => Service['nginx'],
+}
